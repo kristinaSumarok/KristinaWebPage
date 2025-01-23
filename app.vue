461390ae9226f9e14ddb2a4f34ead5784262814b
kristinaSumarok <ktroitski33@gmail.com>
2025-01-23 11:35:03 +0200

    aboutme headline

--- app.vue
+++ app.vue
@@ -1,4 +1,4 @@
 <template>
   <NuxtLayout />
-    <NuxtPage class= "min-h-screen bg-[#8FE8E0]"/>
+    <NuxtPage class= "min-h-screen bg-[#8FE8E0] "/>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="min-h-screen flex flex-grow justify-center py-12">
-      <div class="bg-white rounded-lg shadow-lg p-8 max-w-5xl w-full text-center">
-      <p>text</p>
-    </div>
+      <div class="bg-[#F0FAF5] rounded-lg shadow-xl p-8 max-w-5xl w-full text-center flex flex-col items-center">
+        <div>
+          <h1 class="underline text-3xl">About me</h1>
+        </div>
+      </div>
     </div>
   </template>
