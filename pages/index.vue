fbfced8696c7653d6c2cfba7b8f3238ff39e84e9
kristinaSumarok <ktroitski33@gmail.com>
2025-01-23 13:14:31 +0200

    profile picture

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,13 @@
     <div class="min-h-screen flex flex-grow justify-center py-12">
       <div class="bg-[#F0FAF5] rounded-lg shadow-xl p-8 max-w-5xl w-full text-center flex flex-col items-center">
         <div>
-          <h1 class="underline text-3xl">About me</h1>
+          <h1 class="underline text-3xl pb-8 font-semibold">About me</h1>
+          </div>
+            <div class="rounded-full overflow-hidden aspect-[4/5] max-w-[60%] max-h-[60%]">
+              <img src="assets\profile.jpg" alt="My Photo" class="w-full h-full object-cover" />
+       </div>
+        <div class="py-8 text-xl">
+          <p>Business IT student at Taltech</p>
         </div>
       </div>
     </div>
