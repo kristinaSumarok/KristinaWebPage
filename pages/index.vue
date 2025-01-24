32e7547c08c08fd181d0cc2d01a580b70337c6c6
kristinaSumarok <ktroitski33@gmail.com>
2025-01-24 13:55:31 +0200

    description

--- pages/index.vue
+++ pages/index.vue
@@ -4,11 +4,26 @@
         <div>
           <h1 class="underline text-3xl pb-8 font-semibold">About me</h1>
           </div>
-            <div class="rounded-full overflow-hidden aspect-[4/5] max-w-[60%] max-h-[60%]">
+            <div class="rounded-full overflow-hidden aspect-[4/5] max-w-[40%] max-h-[40%]">
               <img src="assets\profile.jpg" alt="My Photo" class="w-full h-full object-cover" />
        </div>
-        <div class="py-8 text-xl">
-          <p>Business IT student at Taltech</p>
+       <div class="py-5">
+          <p class="text-xl pb-5">Business IT student at Taltech</p>
+          <p class="pb-5">
+            As a second-year Business IT bachelor's student, I am eager to enhance my knowledge in the IT field by contributing 
+            to diverse projects. University has helped me delve deeper into information technology, finance, economics, and programming. 
+            My primary programming language is C#, which I have used throughout my studies. However, I find it valuable to broaden my skills 
+            by learning new languages and frameworks. Currently, I am enhancing my Python proficiency by completing LeetCode exercises and learning 
+            the basics of machine learning using Python libraries such as Keras, NumPy, and pandas.
+          </p>
+          <p class="pb-5">
+            One of the significant courses I have completed is the "Distributed Applications" course, where I gained experience working with APIs and Docker. 
+            Prior to that, I also gained experience developing a monolithic system using .NET MVC.
+          </p>
+          <p class="pb-5">
+            Developing backend solutions is more appealing to me; however, I am open to working as a full-stack engineer. Additionally, I am not only 
+            interested in programming internships, as I would like to gain experience in various aspects of software development to grow into a high-class specialist.
+          </p>
         </div>
       </div>
     </div>
