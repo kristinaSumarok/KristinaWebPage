569990ea375a1fe84b58fc793d300bc3c4cf3db6
kristinaSumarok <ktroitski33@gmail.com>
2025-01-25 15:53:25 +0200

    Updates

---
+++ _nuxt/li8JsWgM.js
@@ -0,0 +1,1 @@
+import{_ as e,x as r,y as t,v as n}from"./Cc4ezs05.js";const s={};function l(a,o){return n(),r("div",null,o[0]||(o[0]=[t("p",null,"portfolio",-1)]))}const f=e(s,[["render",l]]);export{f as default};
