36d53ecf21bcf8d7f648e2dae09fe811885295b6
kristinaSumarok <ktroitski33@gmail.com>
2025-01-25 14:48:52 +0200

    move photo

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="bg-[#00C7BE] py-3 bg-opacity-17" >
       <ul class = "list-none m-0 p-0 flex justify-center">
-        <li><a class="p-4" href = "/">About me</a></li>
+        <li><a class="p-4" href = "index">About me</a></li>
         <li><a class="p-4" href = "portfolio">Portfolio</a></li>
       </ul>
     </div>
